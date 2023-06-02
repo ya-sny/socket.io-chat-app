224257c02884f5f219c79385d473f245826f1eb3
shinya u <shinyahazama7@gmail.com>
2023-06-02 14:57:57 +0900

    mod:URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import {useState} from 'react';
 import Chat from './Chat';
 
-const socket = io("http://localhost:3001");
+const socket = io("https://socket-io-chat-app.herokuapp.com/");
 
 function App() {
   const [userName, setUserName] = useState("");
